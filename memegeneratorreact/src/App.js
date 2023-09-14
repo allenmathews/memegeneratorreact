2c24631bfb81ee30808ca5c426e2a738bab90a6f
Allen Mathews <allenjoemathews@gmail.com>
2023-09-14 20:55:17 +1000

    change

--- memegeneratorreact/src/App.js
+++ memegeneratorreact/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from '../components/header';
 //
 function App() {
-    return ( <div className = "App" >
+    return (
+      <div className = "App" >
         <header className = "App-header" >
         <img src = { logo }
-        className = "App-logo"
-        alt = "logo"/>
+        className = "troll-face"
+        alt = "logo" />
         <p>
-        Edit <code > src / App.js < /code> and save to reload. < /
-        p > <
-        a className = "App-link"
+        The header goes here </p> <a className = "App-link"
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
-        Meme Generator <
-        /a> < /
-        header > <
-        /div>
+          <Header className="header"/>
+        Meme Generator </a> </header > </div>
     );
 }
 
